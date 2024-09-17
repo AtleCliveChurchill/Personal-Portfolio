df8e76c2f92f13ffe36a964e472915927a435e19
AtleCliveChurchill <atleclivechurchill@gmail.com>
2024-09-17 18:45:09 +0900

    final commit

---
+++ src/constants/index.js
@@ -0,0 +1,79 @@
+import project1 from "../assets/projects/project-1.png";
+import project2 from "../assets/projects/project-2.png";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.png";
+import project5 from "../assets/projects/project-5.png";
+
+export const HERO_CONTENT = `A passionate and driven software engineer specializing in Full Stack Development. With a strong foundation in both front-end and back-end technologies, I excel at building dynamic, responsive web applications that deliver seamless user experiences. My expertise includes working with modern frameworks and languages, optimizing performance, and ensuring code quality through best practices. I am dedicated to continuous learning and thrive in collaborative environments where I can contribute to innovative projects that push the boundaries of technology.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. I have worked with a variety of technologies, including React, Node.js, MySQL, PostgreSQL, SpringBoot and 3JS via CesiumJS. My journey in software development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "Feb 2023 - 2023",
+    role: "Software Developer",
+    company: "Fion Systems",
+    description: `Full Stack WebGIS Development by ReactJS integrated with CesiumJS for
+3D Map Rendering and Springboot with PostgreSQL, Development of backend APIs with ExpressJS, Node, Python,  Redesign of Company Website with wordpress, PHPMyAdmin and MySQL`,
+    technologies: ["HTML", "CSS", "JavaScript", "React", "ExpressJS", "CesiumJS", "PHP", "PostgreSQL", "NodeJS", "Python", "Springboot"],
+  },
+  {
+    year: "July 2022 - December 2023",
+    role: "Team Leader",
+    company: "International Writing Center",
+    description: `Mentor students with English communication, writing and resume skills, Spearheaded the Redesign and Front-End modification of Webpage, Managed the scheduling and Back-End of Datasheets, Digital Marketing: Promoted Social Media Reach, Content Creation: Designed creative content and Videos to aid in
+functionality and training`,
+    technologies: ["PHP", "JavaScript", "Office Suite", "Wix", "Adobe", "Wordpress", "Canva"],
+  },
+  {
+    year: "May 2021 - June 2021",
+    role: "Web Development Intern",
+    company: "The Sparks Foundation - India",
+    description: `Complete FrontEnd Design and Animation of Oxygen Donation Page, Demo RazorPay Integration for payment API`,
+    technologies: ["HTML", "CSS", "JavaScript", "Custom RazorPay API"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "React & Framer-Motion WebPage",
+    image: project1,
+    description:
+      "A Responsive WebPage to Showcase personal skills and projects along with personal details and contact information",
+    technologies: ["HTML", "CSS", "React", "TailWind", "JavaScript", "Framer-Motion"],
+  },
+  {
+    title: "React & Material UI Responsive Design Hotel WebDesign",
+    image: project2,
+    description:
+      "A fully responsive webpage using React and Material UI to adapt to different viewports and easy custom design",
+    technologies: ["HTML", "CSS", "React", "MaterialUI", "JavaScript"],
+  },
+  {
+    title: "AI Disease Identification from Potato Leaf Images",
+    image: project3,
+    description:
+      "An AI CNN Model Trained to Identify Blight Diseases among Potato Plants by Uploading the Images through a React Front-End and Fast-API",
+    technologies: ["HTML", "CSS", "JavaScript", "React", "Python", "FastAPI"],
+  },
+  {
+    title: "PHP Cafetaria Website",
+    image: project4,
+    description:
+      "A fully functioning website with HTML, CSS, PHP and Database to store and retreive information",
+    technologies: ["HTML", "CSS", "PHP", "MySQL"],
+  },
+  {
+    title: "Front-End Web Page",
+    image: project5,
+    description:
+      "A basic front-end with vanilla HTML & CSS to grasp the fundamentals, along with Razor-Pay API Intergation ",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+];
+
+export const CONTACT = {
+  address: "Yatap-ro, Bundang-gu, Seongnam-si, Gyeonggi-do, South Korea",
+  phoneNo: "+82 010 6617 5535",
+  email: "atleclivechurchill@gmail.com",
+};
